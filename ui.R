013cd1a599ec2c5ee9210e790bdab42d3ff19224
waroteratan <109091777+waroteratan@users.noreply.github.com>
2022-07-22 20:04:26 +0200

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,41 @@
+suppressWarnings(library(shiny))
+suppressWarnings(library(markdown))
+shinyUI(navbarPage("Capstone: Course Project",
+                   tabPanel("Predict the Next Word",
+                            HTML("<strong>Author: Warote Ratanayanon M S</strong>"),
+                            br(),
+                            HTML("<strong>Date: 22/07/2022</strong>"),
+                            br(),
+                            img(src = "./headers.png"),
+                            # Sidebar
+                            sidebarLayout(
+                              sidebarPanel(
+                                helpText("Enter a partially complete sentence to begin the next word prediction"),
+                                textInput("inputString", "Enter a partial sentence here",value = ""),
+                                br(),
+                                br(),
+                                br(),
+                                br()
+                              ),
+                              mainPanel(
+                                h2("Predicted Next Word"),
+                                verbatimTextOutput("prediction"),
+                                strong("Sentence Input:"),
+                                tags$style(type='text/css', '#text1 {background-color: rgba(255,255,0,0.40); color: blue;}'), 
+                                textOutput('text1'),
+                                br(),
+                                strong("Note:"),
+                                tags$style(type='text/css', '#text2 {background-color: rgba(255,255,0,0.40); color: black;}'),
+                                textOutput('text2')
+                              )
+                            )
+                            
+                   ),
+                   tabPanel("About",
+                            mainPanel(
+                              img(src = "./headers.png"),
+                              includeMarkdown("about.md")
+                            )
+                   )
+)
+)
